7ba7f12c7d0c054e98665aea11d3046491e195c6
kingcrud12 <dipitay@gmail.com>
2026-01-27 19:36:23 +0100

    update framework

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Auomated_ui_test_framework",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
