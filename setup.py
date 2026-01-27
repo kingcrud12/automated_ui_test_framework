1492cc1d1140ab010060a231e32016f69614f039
kingcrud12 <dipitay@gmail.com>
2026-01-27 19:45:39 +0100

    update framework

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Auomated_ui_test_framework",
-    version="0.1.3",
+    version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -15,7 +15,7 @@ setup(
             "automated-ui-framework=automated_ui_test_framework.cli:main",
         ],
     },
-    author="Hugo Testas",
+    author="Yann Dipita",
     description="A POM-based automated UI test framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
